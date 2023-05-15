5f849197a183fde463e06314606fd9ea5667d573
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-05-15 14:52:20 +0200

    Starting up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {useQuery, gql} from '@apollo/client';
+
+function App() {
+
+  return (
+    <>
+     Just trying to build some stuff in here. 🚀
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+
+  cache: new InMemoryCache(),
+
+});
+
+client.query({
+  query:gql`
+  query GetLocations{
+    locations{
+      id
+      name
+      description
+      photo
+    }
+  }`,
+}).then((result) => console.log(result));
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+    <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+)
